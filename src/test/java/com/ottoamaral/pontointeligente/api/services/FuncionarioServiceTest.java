0b22a8f08491915d3cd828d494e55fcdd003575d
Otto Santos <ottoamaraldigital@gmail.com>
2018-01-04 15:20:57 -0200

    atualizando serviço de funcionário

--- src/test/java/com/ottoamaral/pontointeligente/api/services/FuncionarioServiceTest.java
+++ src/test/java/com/ottoamaral/pontointeligente/api/services/FuncionarioServiceTest.java
@@ -1,7 +1,6 @@
 package com.ottoamaral.pontointeligente.api.services;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Optional;
